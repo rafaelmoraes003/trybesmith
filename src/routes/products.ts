b51aebe15a207ddcd0a091409a1a40f25330c042
rafael moraes <rafaelsm003@gmail.com>
2022-09-13 16:45:47 -0300

    feat: cria um endpoint para a listagem de produtos GET /products

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -4,6 +4,7 @@ import ProductsController from '../controllers/products';
 const products = Router();
 const productsController = new ProductsController();
 
+products.get('/', productsController.listAll);
 products.post('/', productsController.create);
 
 export default products;
