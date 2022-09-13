9a1f8023850d1ba4a7a788a864a5a1f7aaf92821
rafael moraes <rafaelsm003@gmail.com>
2022-09-13 17:26:13 -0300

    feat: cria interfaces para tipagem de métodos que envolvem usuários

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -1,13 +1,3 @@
-export interface IProductsBody {
-  name: string,
-  amount: string,
-}
-
-export interface IProducts extends IProductsBody {
-  id: number,
-  userId?: number | null,
-}
-
 export enum StatusCode {
   OK = 200,
   CREATED = 201,
@@ -18,8 +8,33 @@ export enum StatusCode {
   SERVER_ERROR = 500,
 }
 
+/// //////////// PRODUCTS ///////////////////////////////////
+export interface IProductsBody {
+  name: string,
+  amount: string,
+}
+
+export interface IProducts extends IProductsBody {
+  id: number,
+  userId?: number | null,
+}
+
 export interface ProductsServiceData {
   code: StatusCode,
   data?: IProducts | IProducts[],
+  error?: string,
+}
+
+/// ////////////// USERS /////////////////////////////////
+export interface IUserBody {
+  username: string,
+  classe: string,
+  level: number,
+  password: string,
+}
+
+export interface IUserCreated {
+  code: StatusCode,
+  token?: string,
   error?: string,
 }
