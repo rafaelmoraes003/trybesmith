5e5f2d428e2143fb2fb4178de387469deff54ed5
rafael moraes <rafaelsm003@gmail.com>
2022-09-13 16:28:11 -0300

    feat: cria método na classe ProductsService para criar um produto

---
+++ src/services/products.ts
@@ -0,0 +1,19 @@
+import ProductsModel from '../models/products';
+import connection from '../models/connection';
+import { productsSchema } from '../schemas/products';
+import { IProducts, IProductsBody, ProductsServiceData, StatusCode } from '../types/interfaces';
+
+class ProductsService {
+  productsModel = new ProductsModel(connection);
+
+  public create = async (product: IProductsBody): Promise<ProductsServiceData> => {
+    const { error } = productsSchema.validate(product);
+    if (error) {
+      return { code: StatusCode.BAD_REQUEST, error: error.details[0].message };
+    }
+    const newProduct: IProducts = await this.productsModel.create(product);
+    return { code: StatusCode.CREATED, data: newProduct };
+  };
+}
+
+export default ProductsService;
