fa6c9e6a9597d0207deababca2029c30e29a2627
rafael moraes <rafaelsm003@gmail.com>
2022-09-13 16:44:05 -0300

    feat: cria método na classe ProductsController para listar todos os produtos

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -5,6 +5,11 @@ import { IProductsBody } from '../types/interfaces';
 class ProductsController {
   productsService = new ProductsService();
 
+  public listAll = async (_req: Request, res: Response) => {
+    const { code, data } = await this.productsService.listAll();
+    return res.status(code).json(data);
+  };
+
   public create = async (req: Request<unknown, unknown, IProductsBody>, res: Response) => {
     const { name, amount } = req.body;
     const { code, data, error } = await this.productsService.create({ name, amount });
