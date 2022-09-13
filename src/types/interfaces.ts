6c875dfffb07daa538328c1d64ed950ce4f7e01e
rafael moraes <rafaelsm003@gmail.com>
2022-09-13 16:29:14 -0300

    feat: cria arquivo para alocação de interfaces e enums

---
+++ src/types/interfaces.ts
@@ -0,0 +1,24 @@
+export interface IProductsBody {
+  name: string,
+  amount: string,
+}
+
+export interface IProducts extends IProductsBody {
+  id: number,
+}
+
+export enum StatusCode {
+  OK = 200,
+  CREATED = 201,
+  SUCCESS_NO_RESPONSE = 204,
+  BAD_REQUEST = 400,
+  UNAUTHORIZED = 401,
+  NOT_FOUND = 404,
+  SERVER_ERROR = 500,
+}
+
+export interface ProductsServiceData {
+  code: StatusCode,
+  data?: IProducts | IProducts[],
+  error?: string,
+}
