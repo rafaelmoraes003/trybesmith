f023205e299918c5b730524cda08fdf58cf3608e
rafael moraes <rafaelsm003@gmail.com>
2022-09-14 19:36:15 -0300

    refactor: interfaces import products.ts and users.ts

--- src/services/products.ts
+++ src/services/products.ts
@@ -1,23 +1,26 @@
 import ProductsModel from '../models/products';
 import connection from '../models/connection';
 import productsSchema from '../schemas/products';
-import { IProducts, IProductsBody, ProductsServiceData, StatusCode } from '../types/interfaces';
+import { IProducts } from '../types/products';
+import { IServiceResponse } from '../types/serviceResponse';
+import { StatusCode } from '../types/interfaces';
 
 class ProductsService {
   productsModel = new ProductsModel(connection);
 
-  public listAll = async (): Promise<ProductsServiceData> => {
+  public listAll = async (): Promise<IServiceResponse<IProducts[]>> => {
     const products: IProducts[] = await this.productsModel.listAll();
     return { code: StatusCode.OK, data: products };
   };
 
-  public create = async (product: IProductsBody): Promise<ProductsServiceData> => {
+  public create = async (product: IProducts): Promise<IServiceResponse<IProducts>> => {
     const { error } = productsSchema.validate(product);
     if (error) {
-      const status = error.details[0].message.includes('required')
+      const errorMessage: string = error.details[0].message;
+      const status: StatusCode = errorMessage.includes('required')
         ? StatusCode.BAD_REQUEST
         : StatusCode.SEMANTIC_ERROR;
-      return { code: status, error: error.details[0].message };
+      return { code: status, error: errorMessage };
     }
     const newProduct: IProducts = await this.productsModel.create(product);
     return { code: StatusCode.CREATED, data: newProduct };
--- src/services/users.ts
+++ src/services/users.ts
@@ -1,22 +1,24 @@
 import jwt from 'jsonwebtoken';
 import UserModel from '../models/users';
 import connection from '../models/connection';
-import { IUserBody, IUserResponse, StatusCode } from '../types/interfaces';
+import { IUser, IUserAuth } from '../types/users';
 import userSchema from '../schemas/users';
+import { StatusCode } from '../types/interfaces';
 
 const JWT_SECRET = 'supersecretpassword';
 
 class UserService {
   userModel = new UserModel(connection);
 
-  public create = async (user: IUserBody): Promise<IUserResponse> => {
+  public create = async (user: IUser): Promise<IUserAuth> => {
     const { error } = userSchema.validate(user);
     
     if (error) {
-      const status = error.details[0].message.includes('required') 
+      const errorMessage: string = error.details[0].message;
+      const status: StatusCode = errorMessage.includes('required') 
         ? StatusCode.BAD_REQUEST
         : StatusCode.SEMANTIC_ERROR;
-      return { code: status, error: error.details[0].message };
+      return { code: status, error: errorMessage };
     }
     await this.userModel.create(user);
     const token: string = jwt.sign(user, JWT_SECRET);
