b2d27655087de8e0b3cbd79653c86c16ce77d4c0
rafael moraes <rafaelsm003@gmail.com>
2022-09-14 19:25:51 -0300

    feat: cria método na classe OrdersController para criar um novo pedido

--- src/controllers/orders.ts
+++ src/controllers/orders.ts
@@ -1,11 +1,23 @@
 import { Request, Response } from 'express';
 import OrdersService from '../services/orders';
+import { CustomRequest } from '../types/interfaces';
 
 class OrdersController {
   ordersService = new OrdersService();
 
   public listAll = async (_req: Request, res: Response) => {
     const { code, data } = await this.ordersService.listAll();
+    return res.status(code).json(data);
+  };
+
+  public create = async (req: CustomRequest, res: Response) => {
+    const { userId } = req;
+    const { productsIds } = req.body;
+    
+    const { code, data, error } = await this.ordersService.create(userId as number, productsIds);
+    if (error) {
+      return res.status(code).json({ message: error });
+    }
     return res.status(code).json(data);
   };
 }
