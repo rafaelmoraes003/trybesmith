46c57f6d8e54eec0fbf3bb4c1a58bdf3767d0c79
rafael moraes <rafaelsm003@gmail.com>
2022-09-14 19:31:13 -0300

    feat: cria métodos na classe ProductsModel para 1. verificar se productsIds são válidos e 2. atualizar um produto

--- src/models/products.ts
+++ src/models/products.ts
@@ -1,5 +1,5 @@
-import { ResultSetHeader, Pool, RowDataPacket } from 'mysql2/promise';
-import { IProducts, IProductsBody } from '../types/interfaces';
+import { ResultSetHeader, Pool, RowDataPacket, OkPacket } from 'mysql2/promise';
+import { IProducts } from '../types/products';
 
 class ProductsModel {
   private connection: Pool;
@@ -14,7 +14,7 @@ class ProductsModel {
     return products as IProducts[];
   };
 
-  public create = async (product: IProductsBody): Promise<IProducts> => {
+  public create = async (product: IProducts): Promise<IProducts> => {
     const { name, amount } = product;
     const [{ insertId }] = await this.connection.execute<ResultSetHeader>(
       'INSERT INTO Trybesmith.Products (name, amount) VALUES (?, ?)',
@@ -22,6 +22,20 @@ class ProductsModel {
     );
 
     return { id: insertId, name, amount };
+  };
+
+  verifyProducts = async (productsIds: number[]): Promise<boolean> => {
+    const [products] = await this.connection.execute<RowDataPacket[]>(
+      `SELECT * FROM Trybesmith.Products WHERE id IN (${productsIds})`,
+    );    
+    return products.length === productsIds.length;
+  };
+
+  public update = async (productId: number, orderId: number): Promise<void> => {
+    await this.connection.execute<OkPacket>(
+      'UPDATE Trybesmith.Products SET orderId = ? WHERE id = ?',
+      [orderId, productId],
+    );
   };
 }
 
