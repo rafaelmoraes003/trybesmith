84991828d4cac1773763f0d8af87b45dd78a40b6
rafael moraes <rafaelsm003@gmail.com>
2022-09-14 19:34:41 -0300

    feat: adiciona id de usuário na token jwt

--- src/services/login.ts
+++ src/services/login.ts
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken';
 import UserModel from '../models/users';
-import { ILogin, IUserResponse, StatusCode } from '../types/interfaces';
+import { StatusCode } from '../types/interfaces';
+import { ILoginUser, IUserAuth, IUser } from '../types/users';
 import connection from '../models/connection';
 import loginSchema from '../schemas/login';
 
@@ -9,15 +10,18 @@ const JWT_SECRET = 'supersecretpassword';
 class LoginService {
   userModel = new UserModel(connection);
 
-  public login = async (user: ILogin): Promise<IUserResponse> => {
+  public login = async (user: ILoginUser): Promise<IUserAuth> => {
     const { error } = loginSchema.validate(user);
     if (error) {
       return { code: StatusCode.BAD_REQUEST, error: error.details[0].message };
     }
-    if (!(await this.userModel.findUser(user))) {
+
+    const userData: IUser = await this.userModel.findUser(user);
+    
+    if (!userData) {
       return { code: StatusCode.UNAUTHORIZED, error: 'Username or password invalid' };
     }
-    const token = jwt.sign(user, JWT_SECRET);
+    const token: string = jwt.sign({ id: userData.id, ...user }, JWT_SECRET);
     return { code: StatusCode.OK, token };
   };
 }
