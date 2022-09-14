86b5af0f77599dfc9b36383ce2c61fbce0b7bbb4
rafael moraes <rafaelsm003@gmail.com>
2022-09-14 19:33:29 -0300

    feat: cria  um endpoint para o cadastro de um pedido POST /orders

--- src/routes/orders.ts
+++ src/routes/orders.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 import OrdersController from '../controllers/orders';
+import auth from '../middlewares/auth';
 
 const orders = Router();
 const ordersController = new OrdersController();
 
 orders.get('/', ordersController.listAll);
+
+orders.use(auth);
+
+orders.post('/', ordersController.create);
 
 export default orders;
