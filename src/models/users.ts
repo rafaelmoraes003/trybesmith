b016f99eef7ec2a7a2119df4d3dd6b48ad02cea9
rafael moraes <rafaelsm003@gmail.com>
2022-09-14 19:32:32 -0300

    refactor: interface import and params userModel

--- src/models/users.ts
+++ src/models/users.ts
@@ -1,5 +1,5 @@
 import { Pool, ResultSetHeader, RowDataPacket } from 'mysql2/promise';
-import { ILogin, IUser, IUserBody } from '../types/interfaces';
+import { IUser, ILoginUser } from '../types/users';
 
 class UserModel {
   private connection: Pool;
@@ -8,18 +8,18 @@ class UserModel {
     this.connection = connection;
   }
 
-  public findUser = async (myUser: ILogin): Promise<boolean> => {
-    const { username, password } = myUser;
+  public findUser = async (loginUserData: ILoginUser): Promise<IUser> => {
+    const { username, password } = loginUserData;
     const [[findUser]] = await this.connection
       .execute<RowDataPacket[]>(
       'SELECT * FROM Trybesmith.Users WHERE username = ? AND password = ?',
       [username, password],
     );
     const user = findUser as IUser;
-    return !!user;
+    return user;
   };
 
-  public create = async (user: IUserBody): Promise<void> => {
+  public create = async (user: IUser): Promise<void> => {
     const { username, classe, level, password } = user;
     await this.connection.execute<ResultSetHeader>(
       'INSERT INTO Trybesmith.Users (username, classe, level, password) VALUES (?,?,?,?)',
