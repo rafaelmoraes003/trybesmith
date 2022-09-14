6b576cddb079b5182da89e6ea7024a308dc73b2f
rafael moraes <rafaelsm003@gmail.com>
2022-09-14 19:35:23 -0300

    feat: cria método na classe OrdersSerice para criação de pedido

--- src/services/orders.ts
+++ src/services/orders.ts
@@ -1,13 +1,42 @@
 import OrdersModel from '../models/orders';
 import connection from '../models/connection';
-import { IOrderResponse, IOrders, StatusCode } from '../types/interfaces';
+import { StatusCode } from '../types/interfaces';
+import { IOrders } from '../types/orders';
+import { IServiceResponse } from '../types/serviceResponse';
+import productsIdsSchema from '../schemas/orders';
+import updateProducts from '../helpers/updateProducts';
+import verifyProductsIds from '../helpers/verifyProductsIds';
 
 class OrdersService {
   ordersModel = new OrdersModel(connection);
 
-  public listAll = async (): Promise<IOrderResponse> => {
+  public listAll = async (): Promise<IServiceResponse<IOrders[]>> => {
     const orders: IOrders[] = await this.ordersModel.listAll();
     return { code: StatusCode.OK, data: orders };
+  };
+
+  public create = async (
+    userId: number, 
+    productsIds: number[],
+  ): Promise<IServiceResponse<IOrders>> => {
+    const { error } = productsIdsSchema.validate({ productsIds });
+    if (error) {
+      const errorMessage: string = error.details[0].message;
+      const status: StatusCode = errorMessage.includes('required')
+        ? StatusCode.BAD_REQUEST
+        : StatusCode.SEMANTIC_ERROR;
+      return { code: status, error: errorMessage };
+    }
+
+    const validateProductsIds: boolean = await verifyProductsIds(productsIds);
+    if (!validateProductsIds) {
+      return { code: StatusCode.BAD_REQUEST, error: 'Invalid productId' };
+    }
+
+    const orderId: number = await this.ordersModel.create(userId);
+    await updateProducts(orderId, productsIds);
+
+    return { code: StatusCode.CREATED, data: { userId, productsIds } };
   };
 }
 
